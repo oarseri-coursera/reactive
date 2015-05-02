6239edc7d4a1a01cd639b04b863886d41e145342
Oarseri <oarseri@gmail.com>
2015-05-01 21:56:17 -0700

    done with first two mini-milestones

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,26 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal {
+      val aVal: Double = a()
+      val bVal: Double = b()
+      val cVal: Double = c()
+      bVal * bVal - 4.0 * aVal * cVal
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal {
+      val aVal: Double = a()
+      val bVal: Double = b()
+      val dVal = computeDelta(a,b,c)()
+      if (dVal < 0.0) {
+        Set.empty
+      } else {
+        val dRoot: Double = Math.pow(dVal,0.5)
+        Set(dRoot, -1.0 * dRoot).map { r => (r - bVal) / (2.0 * aVal) }
+      }
+    }
   }
 }
