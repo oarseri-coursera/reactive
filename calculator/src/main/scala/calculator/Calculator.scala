597e8373630266a29dacc6bed304beaffd5a61e8
Eric Hsu <ehsu@millennialmedia.com>
2015-05-07 21:48:13 -0400

    finished calculator, with fix for cycles; download next two assignments

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -1,5 +1,7 @@
 package calculator
 
+import scala.collection.mutable.MutableList
+
 sealed abstract class Expr
 final case class Literal(v: Double) extends Expr
 final case class Ref(name: String) extends Expr
@@ -9,15 +11,33 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
+
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.map { case(k,v) =>
+      (k, Signal { eval(getReferenceExpr(k, namedExpressions),
+                        namedExpressions) })
+    }.toMap
   }
 
+  // Given expression and map from variable names to expression signals,
+  // returns double representing the evaluation of expression in that context.
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v: Double) => v
+      case Ref(name: String) =>
+        eval(getReferenceExpr(name, references),
+             references + (name -> Signal(Literal(Double.NaN))))
+      case Plus(a: Expr, b: Expr) => eval(a, references) + eval(b, references)
+      case Minus(a: Expr, b: Expr) => eval(a, references) - eval(b, references)
+      case Times(a: Expr, b: Expr) => eval(a, references) * eval(b, references)
+      case Divide(a: Expr, b: Expr) => eval(a, references) / eval(b, references)
+    }
   }
 
+  // Given variable name and map from variable names to expression signals,
+  // returns expression for that variable if any, or else NaN.  Signal is
+  // "dereferenced" here to yield expression.
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
