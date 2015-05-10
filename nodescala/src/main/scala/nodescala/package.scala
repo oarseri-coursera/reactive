abc50ca4392c19320deda1e0dd20e905f2ed14ed
oarseri <oarseri@gmail.com>
2015-05-10 10:58:44 -0700

    ready to start with server

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -16,33 +16,76 @@ package object nodescala {
   implicit class FutureCompanionOps(val f: Future.type) extends AnyVal {
 
     /** Returns a future that is always completed with `value`.
-     */
-    def always[T](value: T): Future[T] = ???
+      * 
+      * Hint: use a promise.
+      */
+    def always[T](value: T): Future[T] = {
+      val p: Promise[T] = Promise()
+      p.success(value)
+      p.future
+    }
+
     /** Returns a future that is never completed.
-     *
-     *  This future may be useful when testing if timeout logic works correctly.
-     */
-    def never[T]: Future[T] = ???
+      *
+      * This future may be useful when testing if timeout logic works correctly.
+      * 
+      * Hint: use a promise.
+      */
+    def never[T]: Future[T] = {
+      val p: Promise[T] = Promise()
+      // (Never set it with a success or failure.)
+      p.future
+    }
+
     /** Given a list of futures `fs`, returns the future holding the list of values of all the futures from `fs`.
-     *  The returned future is completed only once all of the futures in `fs` have been completed.
-     *  The values in the list are in the same order as corresponding futures `fs`.
-     *  If any of the futures `fs` fails, the resulting future also fails.
-     */
-    def all[T](fs: List[Future[T]]): Future[List[T]] = ???
+      * The returned future is completed only once all of the futures in `fs` have been completed.
+      * The values in the list are in the same order as corresponding futures `fs`.
+      * If any of the futures `fs` fails, the resulting future also fails.
+      * 
+      * Hint: see the lectures.
+      */
+    def all[T](fs: List[Future[T]]): Future[List[T]] = {
+      fs.foldRight(Future.always(List.empty[T])) { (f, flst) =>
+        for {
+          lst <- flst
+          result <- f
+        } yield (result :: lst)
+      }
+    }
+
+
     /** Given a list of futures `fs`, returns the future holding the value of the future from `fs` that completed first.
-     *  If the first completing future in `fs` fails, then the result is failed as well.
-     *
-     *  E.g.:
-     *
-     *      Future.any(List(Future { 1 }, Future { 2 }, Future { throw new Exception }))
-     *
-     *  may return a `Future` succeeded with `1`, `2` or failed with an `Exception`.
-     */
-    def any[T](fs: List[Future[T]]): Future[T] = ???
+      * If the first completing future in `fs` fails, then the result is failed as well.
+      *
+      * E.g.:
+      *
+      *     Future.any(List(Future { 1 }, Future { 2 }, Future { throw new Exception }))
+      *
+      * may return a `Future` succeeded with `1`, `2` or failed with an `Exception`.
+      * 
+      * Hint: use a promise.
+      */
+    def any[T](fs: List[Future[T]]): Future[T] = {
+      val p: Promise[T] = Promise()
+      fs.map { f => f onComplete { res => if (!p.isCompleted) p.complete(res) } }
+      p.future
+    }
 
     /** Returns a future with a unit value that is completed after time `t`.
-     */
-    def delay(t: Duration): Future[Unit] = ???
+      * 
+      * This is the only method that is allowed to be blocking.
+      * It may block the execution thread of this future, but may not block the caller thread.
+      * Along with the 'now' method on futures, is one of only two methods that may use Await.ready/Await.result.
+      * Hint: no (other) hint.
+      */
+    def delay(t: Duration): Future[Unit] = {
+      Future {
+        blocking {
+          Try(Await.ready(Promise().future, t))
+        }
+        Unit
+      }
+    }
 
     /** Completes this future with user input.
      */
@@ -54,7 +97,12 @@ package object nodescala {
 
     /** Creates a cancellable context for an execution and runs it.
      */
-    def run()(f: CancellationToken => Future[Unit]): Subscription = ???
+    def run()(f: CancellationToken => Future[Unit]): Subscription = {
+      val cts = CancellationTokenSource()
+      val ct = cts.cancellationToken
+      f(ct)
+      cts
+    }
 
   }
 
@@ -63,14 +111,22 @@ package object nodescala {
   implicit class FutureOps[T](val f: Future[T]) extends AnyVal {
 
     /** Returns the result of this future if it is completed now.
-     *  Otherwise, throws a `NoSuchElementException`.
-     *
-     *  Note: This method does not wait for the result.
-     *  It is thus non-blocking.
-     *  However, it is also non-deterministic -- it may throw or return a value
-     *  depending on the current state of the `Future`.
-     */
-    def now: T = ???
+      * Otherwise, throws a `NoSuchElementException`.
+      *
+      * Note: This method does not wait for the result.
+      * It is thus non-blocking.
+      * However, it is also non-deterministic -- it may throw or return a value
+      * depending on the current state of the `Future`.
+      *
+      * Along with Future.delay, is one of only two methods that may use Await.ready/Await.result.
+      * Hint: use Await.result to implement this. What should be the time out ?
+      */
+    def now: T = {
+      if (f.isCompleted)
+        Await.result(f, 0 seconds)
+      else
+        throw new NoSuchElementException("Future not completed.")
+    }
 
     /** Continues the computation of this future by taking the current future
      *  and mapping it into another future.
@@ -78,7 +134,9 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      Future { cont(f) }
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
@@ -86,7 +144,11 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continue[S](cont: Try[T] => S): Future[S] = ???
+    def continue[S](cont: Try[T] => S): Future[S] = {
+      val p: Promise[S] = Promise()
+      f onComplete { res => p.complete(Try(cont(res))) }
+      p.future
+    }
 
   }
 
@@ -143,4 +205,3 @@ package object nodescala {
     }
   }
 }
-
--- nodescala/src/test/scala/nodescala/tests.scala
+++ nodescala/src/test/scala/nodescala/tests.scala
@@ -15,12 +15,15 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class NodeScalaSuite extends FunSuite {
 
-  test("A Future should always be completed") {
+  case class MyTestException() extends Exception
+
+  test("Future.always should always be completed") {
     val always = Future.always(517)
 
     assert(Await.result(always, 0 nanos) == 517)
   }
-  test("A Future should never be completed") {
+
+  test("Future.never should never be completed") {
     val never = Future.never[Int]
 
     try {
@@ -31,8 +34,170 @@ class NodeScalaSuite extends FunSuite {
     }
   }
 
-  
-  
+  test("Future.all should return successful results, in right order") {
+    val if1: Future[Int] = Future { Thread.sleep(300); 100 }
+    val if2: Future[Int] = Future { Thread.sleep(200); 200 }
+    val if3: Future[Int] = Future { Thread.sleep(100); 300 }
+
+    val all: Future[List[Int]] = Future.all[Int](List(if2, if3, if1))
+    val correctResult: List[Int] = List(200, 300, 100)
+
+    assert(Await.result(all, 500 millis) == correctResult)
+  }
+
+  test("Future.all should fail if any given future fails") {
+    val if1: Future[Int] = Future { Thread.sleep(300);
+      throw new NoSuchElementException("Born to Fail") }
+    val if2: Future[Int] = Future { Thread.sleep(200); 200 }
+    val if3: Future[Int] = Future { Thread.sleep(100); 300 }
+
+    val all: Future[List[Int]] = Future.all[Int](List(if2, if3, if1))
+
+    try {
+      Await.result(all, 500 millis)
+      assert(false)
+    } catch {
+      case t: NoSuchElementException => // ok!
+    }
+  }
+
+  test("Future.any should return first result to complete (here, success)") {
+    val if1: Future[Int] = Future { Thread.sleep(300)
+      throw new NoSuchElementException("Born to Fail") }
+    val if2: Future[Int] = Future { Thread.sleep(200); 200 }
+    val if3: Future[Int] = Future { Thread.sleep(100); 300 }
+
+    val any: Future[Int] = Future.any[Int](List(if2, if3, if1))
+    val correctResult: Int = 300
+
+    assert(Await.result(any, 500 millis) == correctResult)
+  }
+
+  test("Future.any should return first result to complete (here, failure)") {
+    val if1: Future[Int] = Future { Thread.sleep(300); 100 }
+    val if2: Future[Int] = Future { Thread.sleep(200); 200 }
+    val if3: Future[Int] = Future { Thread.sleep(100);
+      throw new NoSuchElementException("Born to Fail") }
+
+    val any: Future[Int] = Future.any[Int](List(if2, if3, if1))
+
+    try {
+      Await.result(any, 500 millis)
+      assert(false)
+    } catch {
+      case t: NoSuchElementException => // ok!
+    }
+  }
+
+  test("Future.delay should first not complete, then complete") {
+    val delay: Future[Unit] = Future.delay(250 millis)
+    assert(!delay.isCompleted)
+    Thread.sleep(500)
+    assert(delay.isCompleted)
+  }
+
+  // test("Future.delay should not complete after 1s when using a delay of 3s") {
+  //   val delay: Future[Unit] = Future.delay(3 seconds)
+  //   Await.result(delay, 1 seconds)
+  //   assert(!delay.isCompleted)
+  // }
+
+  test("Future.run should let an indefinitely-running computation go until cancelled") {
+    class TestVal(var v: String) {
+      def set(newV: String) { this.synchronized { v = newV } }
+    }
+    val tv = new TestVal("init")
+
+    val working = Future.run() { ct => Future {
+      while (ct.nonCancelled) { tv.set("running") }
+      tv.set("done")
+    }}
+
+    Future.delay(100 millis) onSuccess {
+      case _ => assert(tv.v == "running")
+    }
+
+    Future.delay(200 millis) onSuccess {
+      case _ => assert(tv.v == "running")
+    }
+
+    Future.delay(300 millis) onSuccess {
+      case _ => working.unsubscribe() // sending cancellation
+    }
+
+    Future.delay(400 millis) onSuccess {
+      case _ => assert(tv.v == "done")
+    }
+  }
+
+  test("A future's now should yield result (or exeption) when it's completed") {
+    val f1: Future[Int] = Future { Thread.sleep(100); 99 }
+    val f2: Future[Int] = Future { Thread.sleep(100);
+      throw new MyTestException() }
+
+    Thread.sleep(200)
+
+    assert(f1.now == 99)
+    try {
+      f2.now
+      assert(false)
+    } catch {
+      case t: MyTestException => //ok!
+    }
+  }
+
+  test("A future's now should throw an exception when it's not completed") {
+    val f: Future[Int] = Future { Thread.sleep(100); 99 }
+    try {
+      f.now
+      assert(false)
+    } catch {
+      case t: NoSuchElementException => // ok!
+    }
+  }
+
+  test("A future's continueWith should do the continuation, as a future") {
+    val f: Future[Int] = Future { Thread.sleep(100); 25 }
+    def c(f: Future[Int]): String = {
+      val i: Int = Await.result(f, 200 millis)
+      (i * 2).toString + "!!!"
+    }
+    val cw: Future[String] = f.continueWith(c)
+    assert(!cw.isCompleted)
+    Thread.sleep(250)
+    assert(cw.isCompleted)
+    assert(Await.result(cw, 0 nanos) == "50!!!")
+  }
+
+  test("A future's continue should do the continuation, as a future") {
+    val f: Future[Int] = Future { Thread.sleep(100); 99 }
+    def c(r: Try[Int]): String = r match {
+      case Success(i) => (i * 2).toString + "!!!"
+      case _ => "Did not receive valid int."
+    }
+    val cont: Future[String] = f.continue(c)
+    assert(!cont.isCompleted)
+    Thread.sleep(250)
+    assert(cont.isCompleted)
+    assert(Await.result(cont, 0 nanos) == "198!!!")
+  }
+
+  test("A future's continue should handle exceptions thrown by continuation func") {
+    val f: Future[Int] = Future { Thread.sleep(100); 99 }
+    def c(r: Try[Int]): String = throw new MyTestException()
+    val cont: Future[String] = f.continue(c)
+
+    assert(!cont.isCompleted)
+    Thread.sleep(250)
+    assert(cont.isCompleted)
+    try {
+      Await.result(cont, 0 nanos)
+      assert(false)
+    } catch {
+      case t: MyTestException => //ok!
+    }
+  }
+
   class DummyExchange(val request: Request) extends Exchange {
     @volatile var response = ""
     val loaded = Promise[String]()
